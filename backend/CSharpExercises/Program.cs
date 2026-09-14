91cb022fc86494b29187b1c8952882750ed6afd1
Alexandre Moreira <134660011+Alekson2308@users.noreply.github.com>
2026-09-14 20:57:25 -0300

    Adding "CSharpExercises"

---
+++ backend/CSharpExercises/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace CSharpExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static string GeneratePassword(int length, bool useUppercase, bool useSpecialCharacters)
+            {
+                var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+                if (useUppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                if (useSpecialCharacters) characters += "!?@#$%_-";
+                return string.Concat(Enumerable.Range(0, length).Select(_=> characters[RandomNumberGenerator.GetInt32(characters.Length)]));
+            }
+            Console.WriteLine(GeneratePassword(16, true, true));
+        }
+    }
+}
