fd1142dc29fe4db067aac821a7f01e86b09937eb
Alexandre Moreira <134660011+Alekson2308@users.noreply.github.com>
2026-09-14 19:28:37 -0300

    Add "PasswordGenerator"

---
+++ backend/PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%";
+        Random random = new Random();
+
+        Console.Write("Digite o tamanho da senha: ");
+        int tamanho = int.Parse(Console.ReadLine());
+
+        string senha = "";
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            int indice = random.Next(caracteres.Length);
+            senha += caracteres[indice];
+        }
+
+        Console.WriteLine("\nSenha gerada: " + senha);
+    }
+}
